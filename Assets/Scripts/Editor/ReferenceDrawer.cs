b23b360f6234bffb7bf6644a1535a1e896c42f3a
BrettLennox <brettheaton@hotmail.com>
2022-07-27 16:36:25 +1000

    more

--- Assets/Scripts/Editor/ReferenceDrawer.cs
+++ Assets/Scripts/Editor/ReferenceDrawer.cs
@@ -32,7 +32,45 @@ namespace References
     #endregion
     public class ReferenceDrawer : PropertyDrawer
     {
-        private readonly string[] popupOptions = {"Use Constant", "Use Variable"};
+        private readonly string[] _popupOptions = {"Use Constant", "Use Variable"};
+        private GUIStyle _popupStyle;
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            //basic OnGUI(position, property, label);
+            if (_popupStyle == null)
+            {
+                _popupStyle = new GUIStyle(GUI.skin.GetStyle("PaneOptions"));
+                _popupStyle.imagePosition = ImagePosition.ImageOnly;
+            }
+
+            label = EditorGUI.BeginProperty(position, label, property);
+            position = EditorGUI.PrefixLabel(position, label);
+            
+            EditorGUI.BeginChangeCheck();
+            SerializedProperty useConstant = property.FindPropertyRelative("useConstant");
+            SerializedProperty constantValue = property.FindPropertyRelative("_constantValue");
+            SerializedProperty variable = property.FindPropertyRelative("_variable");
+
+            Rect buttonRect = new Rect(position);
+            buttonRect.yMin += _popupStyle.margin.top;
+            buttonRect.width = _popupStyle.fixedWidth + _popupStyle.margin.right;
+            position.xMin = buttonRect.xMax;
+
+            int _indent = EditorGUI.indentLevel;
+            EditorGUI.indentLevel = 0;
+
+            int _result = EditorGUI.Popup(buttonRect, useConstant.boolValue ? 0:1, _popupOptions, _popupStyle);
+            useConstant.boolValue = _result == 0;
+            EditorGUI.PropertyField(position, useConstant.boolValue ? constantValue : variable, GUIContent.none);
+            if (EditorGUI.EndChangeCheck())
+            {
+                property.serializedObject.ApplyModifiedProperties();
+            }
+
+            EditorGUI.indentLevel = _indent;
+            EditorGUI.EndProperty();
+        }
     }
 }
 
